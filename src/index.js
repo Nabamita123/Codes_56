e5f0d17a15f0f73de264757a995edf615c1d9d66
Nabamita <nabamitabose665@gmail.com>
2025-05-20 13:59:35 +0530

    api response & error

--- src/index.js
+++ src/index.js
@@ -8,6 +8,14 @@ dotenv.config({
 })
 
 connectDb()
+.then(() =>{
+    app.listen(process.env.PORT || 8000 ,()=>{
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((error) =>{
+    console.log("Mongodb connection failed !",error)
+})
 
 
 
