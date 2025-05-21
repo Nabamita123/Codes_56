588a1dea8ec2e5013f536699bee2160a827124da
Nabamita <nabamitabose665@gmail.com>
2025-05-21 13:34:48 +0530

    registration config

--- src/app.js
+++ src/app.js
@@ -7,9 +7,17 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
-
+//middlewares
 app.use(express.json({ limit:"16kb"}))
 app.use(express.urlencoded({extended: true , limit:"16kb"}))
 app.use(express.static("public"))//for assets ex: files
 app.use(cookieParser())
+
+//route import
+
+import userRouter from "./routes/user.routes.js";
+
+//route declaration
+app.use("/api/v1/users", userRouter)
+
 export { app }
