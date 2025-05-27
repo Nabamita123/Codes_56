ca8ba89523e149927cdbb389bdebb6431791d30a
Nabamita <nabamitabose665@gmail.com>
2025-05-27 21:48:55 +0530

    update routes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import {User} from "../models/user.model.js";
 import {uploadOnCloudinary} from "../utils/cloudinary.js";
 import {ApiResponse} from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 const generateAccessandRefreshTokens = async (userId) => {
     try{
       const user =  await User.findById(userId)
@@ -172,7 +173,7 @@ const refreshAccessToken = asyncHandler( async (req, res) =>{
 })
 const changeCurrentPassword = asyncHandler(async(req,res)=>{
     const {oldPassword , newPassword}= req.body
-    const user =User.findById(req.user?._id)
+    const user =await User.findById(req.user?._id)
     const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
 
     if(!isPasswordCorrect){
@@ -186,7 +187,7 @@ const changeCurrentPassword = asyncHandler(async(req,res)=>{
 })
 const getCurrentUser= asyncHandler( async(req,res)=>{
     return res.status(200)
-    .json(200, req.user,"Current User fetched successfully")
+    .json(new ApiResponse(200, req.user,"Current User fetched successfully"))
 })
 
 const updateAccountDetails= asyncHandler(async(req,res)=>{
@@ -258,6 +259,118 @@ const updateUserCoverImage= asyncHandler(async(req,res)=>{
         new ApiResponse(200,user ,"Cover Image Updated successfully")
     )
 })
+
+const getUserChannelProfile = asyncHandler(async(req,res)=>{
+   const{username}= req.params
+   if(!username?.trim()){
+    throw new ApiError(400, "Username is missing")
+   }
+   const channel = await User.aggregate([
+    {
+        $match:{
+            username: username?.toLowerCase()
+
+        }
+    },
+    {
+        $lookup:{
+            from: "subscriptions",
+            localField: "_id",
+            foreignField: "channel",
+            as: "subscribers"
+        }
+    },
+    {
+        $lookup:{
+            from: "subscriptions",
+            localField: "_id",
+            foreignField: "subscriber",
+            as: "subscribedTo"
+        }
+    },
+    {
+        $addFields:{
+            subscribersCount:{
+                $size: "$subscribers"
+            },
+            subscribedToCount:{
+                $size: "$subscribedTo"
+            },
+            isSubscribed:{
+                $cond:{
+                    if:{$in:[req.user?._id, "$subscribers.subscriber"]},
+                    then: true,
+                    else: false
+                }
+            }
+        }
+    },
+    {
+        $project:{
+            fullname:1,
+            username:1,
+            subscribersCount:1,
+            subscribedToCount:1,
+            isSubscribed:1,
+            avatar:1,
+            coverImage:1
+        }
+    }
+   ])
+   if(!channel?.length){
+    throw new ApiError(400,"Channel does not exists")
+   }
+   return res.status(200)
+   .json(new ApiResponse(200,channel[0],"User channel fetched successfully"))
+})
+
+const getWatchHistory = asyncHandler(async (req,res)=>{
+    const user = await User.aggregate([
+        {
+            $match:{
+                
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup:{
+                from:"videos",
+                localField: "watchHistory",
+                foreignField:"_id",
+                as: "watchHistory",
+                pipeline:[
+                    {
+                        $lookup:{
+                            from:"users",
+                            localField:"owner",
+                            foreignField:"_id",
+                            as:"owner",
+                            pipeline:[
+                                {
+                                    $project:{
+                                        fullname:1,
+                                        username:1,
+                                        avatar:1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields:{
+                            owner:{
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+    return res.status(200)
+    .json(new ApiResponse(200,user[0].watchHistory),"Watch History fetched successfully")
+})
+
 export{registerUser,
     loginUser,
     logoutUser,
@@ -266,5 +379,6 @@ export{registerUser,
     getCurrentUser,
     updateAccountDetails,
     updateUserAvatar,
-    updateUserCoverImage
+    updateUserCoverImage,
+    getWatchHistory
 }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser , refreshAccessToken} from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser , refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getWatchHistory} from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -31,5 +31,14 @@ router.route("/register").post(
     router.route("/refresh-token").post(
         refreshAccessToken
     )
+
+    router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+    router.route("/current-user").get(verifyJWT,getCurrentUser)
+    router.route("/update-account-details").patch(verifyJWT,updateAccountDetails)
+    router.route("/update-avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+    router.route("update-cover").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage )
+    router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+    router.route("/watch-history").get(verifyJWT, getWatchHistory)
+    
 
 export default router;
